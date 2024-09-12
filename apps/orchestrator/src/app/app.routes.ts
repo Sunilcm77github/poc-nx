aeb46de6358a0cfdc21ab68eb44da9669b074a4f
Biswajit Jana <beginner2expert4u@gmail.com>
2024-09-12 12:12:47 +0530

    hdhdh

--- apps/orchestrator/src/app/app.routes.ts
+++ apps/orchestrator/src/app/app.routes.ts
@@ -1,24 +1,9 @@
+import { loadRemoteModule } from '@nx/angular/mf';
 import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
-    path: 'projects',
-    children: [
-      {
-        path: '',
-        loadChildren: () =>
-          import('project/Routes').then((m) => m.remoteRoutes),
-      },
-    ],
-  },
-  {
-    path: 'task',
-    children: [
-      {
-        path: '',
-        loadChildren: () =>
-          import('task/Routes').then((m) => m.remoteRoutes),
-      },
-    ],
+    path: '',
+    loadChildren: () => loadRemoteModule('project-remote', './Routes').then(m => m.remoteRoutes),
   },
 ];
